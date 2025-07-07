5816f68c58b27f3fa9a53075784a1e3dcf3fe1b2
anamikagain559 <anamika.zaimahtech@gmail.com>
2025-07-07 20:22:59 +0600

    store added

---
+++ src/redux/store.ts
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from '@/redux/features/counter/counterSlice';
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
+
+// Types for TypeScript
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
