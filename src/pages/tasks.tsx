21bec0b032ef2363934ce814be5e9f5228ae7fd4
anamikagain559 <anamika.zaimahtech@gmail.com>
2025-07-09 15:25:08 +0600

    pages added

---
+++ src/pages/tasks.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function tasks() {
+  return (
+    <div>tasks</div>
+  )
+}
+
+export default tasks
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
+import App from "@/App";
 import { createBrowserRouter } from "react-router";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <div>Home Page</div>,
+        Component: App,
     }
 ]);
 
